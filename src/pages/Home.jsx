1a8e64c9591c2578f402f657a3e85a109a109c06
Ivan <68665931+Eir-Rithsen@users.noreply.github.com>
2025-10-23 23:20:43 -0300

    Add Home component with welcome message

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function Home() {
+return (
+<section>
+<h1>Bienvenido a la Pre-Entrega</h1>
+<p>Proyecto eCommerce básico en React. Ve a Productos para comenzar.</p>
+</section>
+)
+}
